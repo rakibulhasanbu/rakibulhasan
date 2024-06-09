bd5eed777f9351a4ef5c774bc943eee378ed1013
rakibulhasanbu <rakibulhasanbu@gmail.com>
2024-06-09 19:57:23 +0600

    Update informations

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "rakibulhasanbu@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
@@ -147,7 +147,7 @@ export const BentoGridItem = ({
                 {leftLists.map((item, i) => (
                   <span
                     key={i}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50
                     lg:opacity-100 rounded-lg text-center bg-[#10132E]"
                   >
                     {item}
@@ -160,7 +160,7 @@ export const BentoGridItem = ({
                 {rightLists.map((item, i) => (
                   <span
                     key={i}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50
                     lg:opacity-100 rounded-lg text-center bg-[#10132E]"
                   >
                     {item}
