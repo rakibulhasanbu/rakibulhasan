37d4544f3055d1c1882477f867b47f161b2f519c
rakibulhasanbu <rakibulhasanbu@gmail.com>
2024-06-09 17:49:44 +0600

    updated by npm install

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 const svgToDataUri = require("mini-svg-data-uri");
 
 const colors = require("tailwindcss/colors");
+
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
